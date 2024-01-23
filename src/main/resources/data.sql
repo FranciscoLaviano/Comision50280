25427edcc72ff54c7f4e81f0b7a8dd530dfd9d16
Francisco Laviano <linespi39@gmail.com>
2024-01-23 00:27:05 -0300

    subiendo proyecto en java

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,9 +6,9 @@
   (5, 'Elena', ' ', 'Laviano', ' ', 29, 5.0),
   (6, 'Mauricio', 'Gonzalo', 'Pereira', 'Alvez', 20, 6.0)
 
-  INSERT INTO grupo7(idAlumno, primerNombre, segundoNombre, primerApellido, segundoApellido, edad, nota) VALUES
-   (1, 'Nicolas', ' ', ' Sapriza', 'Brignoni', 24, 12),
-   (2, 'Yoselin', ' ', 'Prado', ' ', 21, 12.0),
-   (3, 'Francisco', 'Antonio ', 'Laviano', 'Colman', 39, 7.6),
-   (4, 'Maria', 'Alejandra Pia', 'Moreira', 'Lapauble ', 43, 9.5),
-   (5, 'Claudio', ' Ernesto', 'Flores', ' ', 19, 8.0)
+ -- INSERT INTO grupo7(idAlumno, primerNombre, segundoNombre, primerApellido, segundoApellido, edad, nota) VALUES
+ --  (1, 'Nicolas', ' ', ' Sapriza', 'Brignoni', 24, 12),
+ --  (2, 'Yoselin', ' ', 'Prado', ' ', 21, 12.0),
+  -- (3, 'Francisco', 'Antonio ', 'Laviano', 'Colman', 39, 7.6),
+ --  (4, 'Maria', 'Alejandra Pia', 'Moreira', 'Lapauble ', 43, 9.5),
+ --  (5, 'Claudio', ' Ernesto', 'Flores', ' ', 19, 8.0)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,27 +5,28 @@
 
  -- USE alumnado; 
 
-CREATE TABLE grupo6(
+  -- CREATE TABLE grupo6(
     
-    idAlumno INT PRIMARY KEY,
-    primerNombre VARCHAR(16) NOT NULL,
-    segundoNombre VARCHAR(60),
-    primerApellido VARCHAR(16) NOT NULL,
-    segundoApellido VARCHAR(16),
-    edad INT,
-    nota FLOAT
+ --   idAlumno INT PRIMARY KEY,
+ --   primerNombre VARCHAR(16) NOT NULL,
+ --   segundoNombre VARCHAR(60),
+ --   primerApellido VARCHAR(16) NOT NULL,
+ --   segundoApellido VARCHAR(16),
+  --  edad INT,
+  --  nota FLOAT
 
 
 
-);
-CREATE TABLE grupo7(
+-- );
 
-   idAlumno INT PRIMARY KEY,
-   primerNombre VARCHAR(16) NOT NULL,
-   segundoNombre VARCHAR(60),
-   primerApellido VARCHAR(16) NOT NULL,
-   segundoApellido VARCHAR(16),
-   edad INT,
-   nota FLOAT
+ -- CREATE TABLE grupo7(
 
-);
+ --  idAlumno INT PRIMARY KEY,
+ --  primerNombre VARCHAR(16) NOT NULL,
+ --  segundoNombre VARCHAR(60),
+ --  primerApellido VARCHAR(16) NOT NULL,
+ --  segundoApellido VARCHAR(16),
+ --  edad INT,
+ --  nota FLOAT
+
+ -- );
